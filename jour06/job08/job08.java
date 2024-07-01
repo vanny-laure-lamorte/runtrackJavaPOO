2f0c49ba8ca7560b176a83863d2323fe442d9905
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-07-01 12:30:43 +0200

    (feat)job08

---
+++ jour06/job08/job08.java
@@ -0,0 +1,16 @@
+package jour06.job08;
+
+public class job08 {
+
+    public static void main(String[] args) {
+
+        double somme = 0;
+        
+        for (int i=1; i<101; i++) {
+            somme = somme + i; 
+        }
+
+        System.out.println(somme);
+    }
+    
+}

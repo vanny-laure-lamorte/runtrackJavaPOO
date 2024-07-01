d57865f60f59b9f19acfa91be615d8dd7f15ebe4
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-07-01 10:31:09 +0200

    (feat)job01

---
+++ jour06/job01/job01.java
@@ -0,0 +1,15 @@
+package jour06.job01;
+import java.util.Scanner;
+
+
+public class job01 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); 
+        System.out.print("Ecrivez un mot: ");
+        String inputUser = input.nextLine();
+        System.out.println("Vous avez saisi : " + inputUser);
+        input.close();        
+    }
+    
+}

b3ae2005dc1c2d998165dc88f2b680214c2b3687
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-07-01 12:51:09 +0200

    (feat)job11

---
+++ jour06/job11/job11.java
@@ -0,0 +1,15 @@
+package jour06.job11;
+
+public class job11 {
+    public static void main(String[] args) {
+
+    int resultat = 1;
+
+        for (int i = 1; i < 9; i++) {
+            resultat = resultat * i; 
+        }
+
+        System.out.println(resultat);
+    }
+    
+}
